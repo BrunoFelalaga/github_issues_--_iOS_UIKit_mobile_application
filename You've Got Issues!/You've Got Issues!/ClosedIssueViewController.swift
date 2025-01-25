323ac4235f03911e35b983d0363f1f346b0e6be5
BrunoFelalaga <felalagabruno@gmail.com>
2025-01-25 17:25:14 -0600

    ---

--- You've Got Issues!/You've Got Issues!/ClosedIssueViewController.swift
+++ You've Got Issues!/You've Got Issues!/ClosedIssueViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ClosedIssueViewController: UITableViewController {
     let issues: [String] = []
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowIssueDetail",
+        if segue.identifier == "ShowIssueDetail" {
            let destination = segue.destination as? IssuesDetailViewController,
            let issue = issues[selectedIndexPath.row]
            destination.issue = issue
